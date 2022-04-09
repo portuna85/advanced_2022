a732b7044f3bee9429850278457972aa667689ba
portuna85 <portuna85@gmail.com>
2022-04-09 17:27:01 +0900

    TemplateMethod Update

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,9 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -31,4 +35,50 @@ public class ContextV1Test {
         log.info("resultTime = {}", resultTime);
     }
 
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogic1 logic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(logic1);
+        contextV1.execute();
+
+        StrategyLogic2 logic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(logic2);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+
+
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+
+
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV4() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비지니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비지니스 로직1 실행"));
+        context2.execute();
+    }
 }
